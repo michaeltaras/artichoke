05e32825baeb8794c8e5ec1f97743f3ece65b464
Artichoke CI <ci@artichokeruby.org>
2020-02-10 04:41:23 +0000

    [skip ci] build docs

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,10 +1,4 @@
 (function() {var implementors = {};
 implementors["artichoke_backend"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"artichoke_backend/gc/struct.ArenaIndex.html\" title=\"struct artichoke_backend::gc::ArenaIndex\">ArenaIndex</a>","synthetic":false,"types":["artichoke_backend::gc::ArenaIndex"]}];
 implementors["artichoke_frontend"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"artichoke_frontend/parser/struct.Parser.html\" title=\"struct artichoke_frontend::parser::Parser\">Parser</a>","synthetic":false,"types":["artichoke_frontend::parser::Parser"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/downcast/trait.Downcast.js
+++ implementors/downcast/trait.Downcast.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["artichoke_backend"] = [{"text":"impl&lt;_T:&nbsp;Any&gt; Downcast&lt;_T&gt; for dyn <a class=\"trait\" href=\"artichoke_backend/extn/core/array/backend/trait.ArrayType.html\" title=\"trait artichoke_backend::extn::core::array::backend::ArrayType\">ArrayType</a> <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;_T: Any,&nbsp;</span>","synthetic":false,"types":["artichoke_backend::extn::core::array::backend::ArrayType"]},{"text":"impl&lt;_T:&nbsp;Any&gt; Downcast&lt;_T&gt; for dyn <a class=\"trait\" href=\"artichoke_backend/extn/core/random/backend/trait.RandType.html\" title=\"trait artichoke_backend::extn::core::random::backend::RandType\">RandType</a> <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;_T: Any,&nbsp;</span>","synthetic":false,"types":["artichoke_backend::extn::core::random::backend::RandType"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
