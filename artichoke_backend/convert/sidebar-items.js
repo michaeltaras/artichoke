0b4b3775d8b5d992da0cb94b5e476a0e50d8932a
Artichoke CI <ci@artichokeruby.org>
2020-02-12 17:22:23 +0000

    [skip ci] build docs

--- artichoke_backend/convert/sidebar-items.js
+++ artichoke_backend/convert/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["RustBackedValue","Provides converters to and from [`Value`] with ruby type of [`Ruby::Data`]."]]});
+initSidebarItems({"struct":[["BoxIntoRubyError","Failed to convert from Rust type to a boxed Ruby value."],["UnboxRubyError","Failed to convert from boxed Ruby value to a Rust type."]],"trait":[["RustBackedValue","Provides converters to and from [`Value`] with ruby type of [`Ruby::Data`]."]]});
