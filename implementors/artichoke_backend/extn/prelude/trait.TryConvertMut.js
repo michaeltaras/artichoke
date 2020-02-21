b3eead419b89a1305eb44ad26170ac86a9ed82b9
artichoke-ci <ci@artichokeruby.org>
2020-02-21 04:23:23 +0000

    deploy: bc7539049326d9821863c67f56861e21b0ce79e5

--- implementors/artichoke_backend/extn/prelude/trait.TryConvertMut.js
+++ implementors/artichoke_backend/extn/prelude/trait.TryConvertMut.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["artichoke_backend"] = [{"text":"impl&lt;T, U&gt; <a class=\"trait\" href=\"artichoke_backend/extn/prelude/trait.TryConvertMut.html\" title=\"trait artichoke_backend::extn::prelude::TryConvertMut\">TryConvertMut</a>&lt;T, U&gt; for <a class=\"struct\" href=\"artichoke_backend/struct.Artichoke.html\" title=\"struct artichoke_backend::Artichoke\">Artichoke</a> <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;<a class=\"struct\" href=\"artichoke_backend/struct.Artichoke.html\" title=\"struct artichoke_backend::Artichoke\">Artichoke</a>: <a class=\"trait\" href=\"artichoke_backend/extn/prelude/trait.ConvertMut.html\" title=\"trait artichoke_backend::extn::prelude::ConvertMut\">ConvertMut</a>&lt;T, U&gt;,&nbsp;</span>","synthetic":false,"types":["artichoke_backend::Artichoke"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
