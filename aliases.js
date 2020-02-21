b1a0ca72e861f9bcec69e26f4ae668456a42d52f
Artichoke CI <ci@artichokeruby.org>
2020-02-21 03:46:36 +0000

    [skip ci] build docs

--- aliases.js
+++ aliases.js
@@ -4,5 +4,4 @@ ALIASES["artichoke"] = {};
 ALIASES["artichoke_backend"] = {};
 ALIASES["artichoke_core"] = {};
 ALIASES["artichoke_frontend"] = {};
-ALIASES["ruby"] = {};
 ALIASES["spec_runner"] = {};
