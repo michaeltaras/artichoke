f0fae320e16a48a3f8c45dbf4b9249fcf8e51a8e
artichoke-ci <ci@artichokeruby.org>
2020-03-28 01:01:58 +0000

    deploy: 2bfef8f77cfb195c23e0629967b25014c4a7ec73

--- artichoke_backend/types/sidebar-items.js
+++ artichoke_backend/types/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Ruby","Classes of Ruby types."],["Rust","Classes of Rust types."]],"fn":[["ruby_from_mrb_value",""]],"type":[["Float",""],["Int",""]]});
+initSidebarItems({"enum":[["Ruby","Classes of Ruby types."],["Rust","Classes of Rust types."]],"fn":[["ruby_from_mrb_value","Parse a [`Ruby`] type classifier from a [`sys::mrb_value`]."]],"type":[["Float",""],["Int",""]]});
