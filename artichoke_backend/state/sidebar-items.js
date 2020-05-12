0ade25dd2268b7f1b3372a5bb1893e7a2c8200d9
artichoke-ci <ci@artichokeruby.org>
2020-05-12 04:53:54 +0000

    deploy: 52d8be75ac7cc99263156a58c3bb036cbc0cf29b

--- artichoke_backend/state/sidebar-items.js
+++ artichoke_backend/state/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["output",""],["parser",""],["prng",""],["regexp",""],["type_registry",""]],"struct":[["State",""]]});
+initSidebarItems({"mod":[["output",""],["parser",""],["prng",""],["regexp",""],["type_registry",""]],"struct":[["State","Container for domain-specific interpreter state."]]});
