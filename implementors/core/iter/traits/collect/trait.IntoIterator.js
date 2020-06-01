4ba080f54a8a2533c407a9ccb28a154f01a1b0f6
artichoke-ci <ci@artichokeruby.org>
2020-06-01 13:51:12 +0000

    deploy: 464935523f2677695d7f04f83bcde443c07eb16b

---
+++ implementors/core/iter/traits/collect/trait.IntoIterator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["artichoke_backend"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for &amp;'a <a class=\"struct\" href=\"artichoke_backend/extn/core/array/struct.Array.html\" title=\"struct artichoke_backend::extn::core::array::Array\">Array</a>","synthetic":false,"types":["artichoke_backend::extn::core::array::Array"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/core/iter/traits/iterator/trait.Iterator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["artichoke_backend"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"artichoke_backend/extn/core/array/struct.Iter.html\" title=\"struct artichoke_backend::extn::core::array::Iter\">Iter</a>&lt;'a&gt;","synthetic":false,"types":["artichoke_backend::extn::core::array::Iter"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
