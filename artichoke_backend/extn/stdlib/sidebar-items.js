b147825da736e854faae2e3f0db42f9140c15452
artichoke-ci <ci@artichokeruby.org>
2020-06-08 14:46:54 +0000

    deploy: a589806711069c4f03f15135815b0a3df724b349

--- artichoke_backend/extn/stdlib/sidebar-items.js
+++ artichoke_backend/extn/stdlib/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["init",""]],"mod":[["abbrev",""],["cmath",""],["delegate",""],["forwardable",""],["json",""],["monitor",""],["ostruct",""],["securerandom",""],["set",""],["shellwords",""],["strscan",""],["time",""],["uri",""]]});
+initSidebarItems({"fn":[["init",""]],"mod":[["abbrev",""],["base64",""],["cmath",""],["delegate",""],["forwardable",""],["json",""],["monitor",""],["ostruct",""],["securerandom",""],["set",""],["shellwords",""],["strscan",""],["time",""],["uri",""]]});
