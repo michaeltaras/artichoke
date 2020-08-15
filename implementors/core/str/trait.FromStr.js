45ec397350b6f28d2549eb4187a70871b1313bf6
artichoke-ci <ci@artichokeruby.org>
2020-08-15 18:35:44 +0000

    deploy: f404cd0c8ec26ca71e51c4cfef124900e541b3e3

--- implementors/core/str/trait.FromStr.js
+++ implementors/core/str/trait.FromStr.js
@@ -5,6 +5,7 @@ implementors["log"] = [{"text":"impl FromStr for Level","synthetic":false,"types
 implementors["nix"] = [{"text":"impl FromStr for Signal","synthetic":false,"types":[]}];
 implementors["proc_macro2"] = [{"text":"impl FromStr for TokenStream","synthetic":false,"types":[]}];
 implementors["regex"] = [{"text":"impl FromStr for Regex","synthetic":false,"types":[]},{"text":"impl FromStr for Regex","synthetic":false,"types":[]}];
+implementors["spinoso_symbol"] = [{"text":"impl FromStr for IdentifierType","synthetic":false,"types":[]}];
 implementors["termcolor"] = [{"text":"impl FromStr for Color","synthetic":false,"types":[]}];
 implementors["uuid"] = [{"text":"impl FromStr for Uuid","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
