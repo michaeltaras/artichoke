71100ef9cb2c71839043cc5c61b6e956be6a773d
artichoke-ci <ci@artichokeruby.org>
2020-09-04 01:45:05 +0000

    deploy: 0170a2d9051a32860e0a2746092762ee49fdc6e3

--- artichoke_core/top_self/sidebar-items.js
+++ artichoke_core/top_self/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["TopSelf","Return a `Value`-wrapped reference to [top self][topself]."]]});
+initSidebarItems({"trait":[["TopSelf","Return a `Value`-wrapped reference to top self."]]});
