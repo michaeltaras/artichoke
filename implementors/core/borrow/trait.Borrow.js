a37f796940531f49a470a8b1667e72f2d5788803
artichoke-ci <ci@artichokeruby.org>
2020-11-23 19:41:53 +0000

    deploy: ed2a81a14af86cb1d1d6c114c2a26749c984bc87

--- implementors/core/borrow/trait.Borrow.js
+++ implementors/core/borrow/trait.Borrow.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["artichoke_backend"] = [{"text":"impl Borrow&lt;Error&gt; for WriteError","synthetic":false,"types":[]}];
 implementors["bstr"] = [{"text":"impl Borrow&lt;BStr&gt; for BString","synthetic":false,"types":[]}];
 implementors["smallvec"] = [{"text":"impl&lt;A:&nbsp;Array&gt; Borrow&lt;[&lt;A as Array&gt;::Item]&gt; for SmallVec&lt;A&gt;","synthetic":false,"types":[]}];
 implementors["spinoso_array"] = [{"text":"impl&lt;T&gt; Borrow&lt;[T]&gt; for SmallArray&lt;T&gt;","synthetic":false,"types":[]},{"text":"impl&lt;T&gt; Borrow&lt;[T]&gt; for Array&lt;T&gt;","synthetic":false,"types":[]}];
--- implementors/core/borrow/trait.BorrowMut.js
+++ implementors/core/borrow/trait.BorrowMut.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["artichoke_backend"] = [{"text":"impl BorrowMut&lt;Error&gt; for WriteError","synthetic":false,"types":[]}];
 implementors["smallvec"] = [{"text":"impl&lt;A:&nbsp;Array&gt; BorrowMut&lt;[&lt;A as Array&gt;::Item]&gt; for SmallVec&lt;A&gt;","synthetic":false,"types":[]}];
 implementors["spinoso_array"] = [{"text":"impl&lt;T&gt; BorrowMut&lt;[T]&gt; for SmallArray&lt;T&gt;","synthetic":false,"types":[]},{"text":"impl&lt;T&gt; BorrowMut&lt;[T]&gt; for Array&lt;T&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
