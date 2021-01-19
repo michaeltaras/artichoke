7ab63b39f8fccb5a3011aa5f8bf85f1273ef12d3
artichoke-ci <ci@artichokeruby.org>
2021-01-19 02:22:41 +0000

    deploy: a80134a4eb8782adb37acc9cab5abdd06dda6d15

--- artichoke_backend/extn/core/array/trampoline/sidebar-items.js
+++ artichoke_backend/extn/core/array/trampoline/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["clear",""],["concat",""],["element_assignment",""],["element_reference",""],["first",""],["initialize",""],["initialize_copy",""],["last",""],["len",""],["plus",""],["pop",""],["push",""],["reverse",""],["reverse_bang",""],["shift",""]]});
+initSidebarItems({"fn":[["clear",""],["concat",""],["element_assignment",""],["element_reference",""],["first",""],["initialize",""],["initialize_copy",""],["last",""],["len",""],["mul",""],["plus",""],["pop",""],["push",""],["reverse",""],["reverse_bang",""],["shift",""]]});
